c6c68e410a5e35e7ceb2df0388c835721d2da1bf
Ercion <37574165+Ercion@users.noreply.github.com>
2018-07-09 22:28:06 +0300

    Add files via upload
    This is first lesson for python's basics

---
+++ lesson_one.py
@@ -0,0 +1,65 @@
+##ERCANKARACELIK##
+#for comment use # mark 
+
+
+x='example'
+x
+x.upper()
+print(x)
+
+
+## 1 -> intger & 1.0 -> float 
+type(1)
+type(1.0)
+
+## Basic Math in Python
+#Aggregation
+1+1
+#subtraction
+8-5
+#Asteriks
+1*4
+#Exponent
+2**3
+#Mod
+4%2
+7%3
+
+#Let's try it
+2+3*5+5
+
+#Let's try do same thing with parentheses
+(2+3)*(5+5)
+
+##Defining Variable
+##Python prefers snake casing. It means If you have multiple words inside a variable name that you seperate them out by underscores.
+
+my_var_name=123
+
+#defining variables and assignments
+x=1
+y=2
+
+#Aggregate then
+print(x+y)
+
+##We talked about numbers and variable assignment
+##Let's quickly talk about strings. You can write 'Strings' or "Strings". There is no matter.  But if you want to write something like that 'Hey, It's my code'. It's getting confused Because python doesn't understand where your actual strings start and end.
+
+"Hey, It's my code" #It works perfectly.
+
+#Let's print
+
+print('hello')
+print("hello")
+
+#there is no problem as you see
+
+#How to format your prints
+print("My name is {}".format('ERCANKARACELIK'))
+
+print("My name is {} and my favorite number is {}".format('Ercan','20'))
+
+#attention,in format method, I use number in quotes
+ 
+
